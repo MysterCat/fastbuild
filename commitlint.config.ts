44c354a820309ab5a58ba88b2a0c97e74e23281d
李成豪 <2815261401@qq.com>
2026-07-01 09:01:08 +0800

    🔧 chore: 🔥移除对`@commitlint/config-conventional`的引用

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,6 @@
 import type { UserConfig } from '@commitlint/types'
 import type { CommitType } from 'conventional-changelog-conventionalcommits'
 
-import defaultConfig from '@commitlint/config-conventional'
 import createPreset from 'conventional-changelog-conventionalcommits'
 
 /** 类型枚举 */
@@ -63,7 +62,66 @@ export const types: CommitType[] = [
   },
 ]
 
-const typeEnum = defaultConfig.prompt.questions.type.enum
+const typeEnum = {
+  feat: {
+    description: 'A new feature',
+    title: 'Features',
+    emoji: '✨',
+  },
+  fix: {
+    description: 'A bug fix',
+    title: 'Bug Fixes',
+    emoji: '🐛',
+  },
+  docs: {
+    description: 'Documentation only changes',
+    title: 'Documentation',
+    emoji: '📚',
+  },
+  style: {
+    description:
+      'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+    title: 'Styles',
+    emoji: '💎',
+  },
+  refactor: {
+    description: 'A code change that neither fixes a bug nor adds a feature',
+    title: 'Code Refactoring',
+    emoji: '📦',
+  },
+  perf: {
+    description: 'A code change that improves performance',
+    title: 'Performance Improvements',
+    emoji: '🚀',
+  },
+  test: {
+    description: 'Adding missing tests or correcting existing tests',
+    title: 'Tests',
+    emoji: '🚨',
+  },
+  build: {
+    description:
+      'Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
+    title: 'Builds',
+    emoji: '🛠',
+  },
+  ci: {
+    description:
+      'Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)',
+    title: 'Continuous Integrations',
+    emoji: '⚙️',
+  },
+  chore: {
+    description: 'Other changes that don\'t modify src or test files',
+    title: 'Chores',
+    emoji: '♻️',
+  },
+  revert: {
+    description: 'Reverts a previous commit',
+    title: 'Reverts',
+    emoji: '🗑',
+  },
+}
 
 /** 创建 Emoji 解析器 */
 function createEmojiParser() {
